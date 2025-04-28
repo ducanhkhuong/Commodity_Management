178855913481dd60c0866d813585a75e3d259726
@anhkd <124881820+ducanhkhuong@users.noreply.github.com>
2025-04-28 11:02:43 +0700

    merge Session (#1)
    * style: change folder name
    
    * refactor: add manager task forward for uiux & add groupbox
    
    * feat :hardware structure

---
+++ firmware/desktop/EmguCV-CameraCapture/CameraCapture/CameraCapture.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SessionManager
+{
+    partial class SessionManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SessionManager));
+            this.CamImageBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.datamock = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CamImageBox
+            // 
+            this.CamImageBox.BackColor = System.Drawing.SystemColors.Window;
+            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImageBox.Location = new System.Drawing.Point(24, 29);
+            this.CamImageBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CamImageBox.Name = "CamImageBox";
+            this.CamImageBox.Size = new System.Drawing.Size(610, 442);
+            this.CamImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CamImageBox.TabIndex = 2;
+            this.CamImageBox.TabStop = false;
+            this.CamImageBox.Click += new System.EventHandler(this.CamImageBox_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(24, 65);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // datamock
+            // 
+            this.datamock.Location = new System.Drawing.Point(34, 71);
+            this.datamock.Name = "datamock";
+            this.datamock.Size = new System.Drawing.Size(100, 22);
+            this.datamock.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox1.Controls.Add(this.CamImageBox);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(30, 171);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(653, 492);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camera";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(500, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(574, 76);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Mô hình quản lý và phân loại hàng hóa\r\n\r\n";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox2.Location = new System.Drawing.Point(30, 64);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 100);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kết nối";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox3.Controls.Add(this.datamock);
+            this.groupBox3.Location = new System.Drawing.Point(728, 64);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(883, 599);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kho hàng";
+            // 
+            // SessionManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1642, 700);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "SessionManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Camera Session";
+            this.Load += new System.EventHandler(this.CameraCapture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox datamock;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ firmware/desktop/EmguCV-CameraCapture/CameraCapture/CameraCapture.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using ZXing;
+using ZXing.Common;
+using ZXing.Presentation;
+using ZXing.QrCode.Internal;
+
+namespace SessionManager
+{
+    public partial class SessionManager : Form
+    {
+        private Capture capture;
+        private bool captureInProgress;
+        private ZXing.BarcodeReader barcodeReader;
+        private string lastQrCode;
+        private CancellationTokenSource cancellationTokenSource;
+        
+        private ConcurrentQueue<string> processPushQueue;
+        private ConcurrentQueue<string> processPullQueue;
+        private ConcurrentQueue<string> processUIUXQueue;
+        private ConcurrentQueue<string> processForward;
+        
+        private Task HandllerPushTask;
+        private Task HandllerUITask;
+        private Task HandllerPullTask;
+        private Task HandllerForward;
+
+        public SessionManager()
+        {
+            InitializeComponent();
+            processPushQueue        = new ConcurrentQueue<string>();
+            processPullQueue        = new ConcurrentQueue<string>();
+            processUIUXQueue        = new ConcurrentQueue<string>();
+            processForward          = new ConcurrentQueue<string>();
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                capture = new Capture(1);
+                barcodeReader = new ZXing.BarcodeReader
+                {
+                    AutoRotate = true,
+                    Options = new DecodingOptions
+                    {
+                        TryHarder = true,
+                        PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.QR_CODE }
+                    }
+                };
+
+                HandllerPushTask = Task.Run(() => ProcessPushHandller(cancellationTokenSource.Token));
+                HandllerUITask   = Task.Run(() => ProcessUI(cancellationTokenSource.Token));
+                HandllerPullTask = Task.Run(() => ProcessPullHandller(cancellationTokenSource.Token));
+                HandllerForward  = Task.Run(() => ProcessForwardHandller(cancellationTokenSource.Token));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khởi tạo camera hoặc barcodeReader: {ex.Message}");
+            }
+        }
+
+        private async void ProcessForwardHandller(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if(processForward.TryDequeue(out string qrCode))
+                {
+                    //processPushQueue.Enqueue(result.Text);
+                    //processUIUXQueue.Enqueue(result.Text);
+                }
+                await Task.Delay(500);
+            }
+        }
+
+        private async void ProcessPullHandller(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                //nhận dữ liệu phản hồi mỗi 0.5 giây là các byte hex , phân tích dữ liệu và lưu vào DB
+                Console.WriteLine($"task pull");
+                await Task.Delay(500);
+            }
+        }
+
+        private async void ProcessPushHandller(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if (processPushQueue.TryDequeue(out string qrCode))
+                {
+                    //xử lý mã qr , ép về byte hex , gửi dữ liệu đi 
+                    Console.WriteLine($"ProcessHandller from queue: {qrCode}");
+                }
+                await Task.Delay(100);
+            }
+        }
+
+        private async void ProcessUI(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if (processUIUXQueue.TryDequeue(out string qrCode))
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        //xử lý ui ux giao diện 
+                        datamock.Text = $"{qrCode}";
+                        Console.WriteLine($"ProcessUIUX from queue: {qrCode}");
+                    });
+                    await Task.Delay(200);
+                }
+                await Task.Delay(100);
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            try
+            {
+                if (capture == null)
+                {
+                    return;
+                }
+
+                Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
+                if (ImageFrame == null || ImageFrame.Width == 0 || ImageFrame.Height == 0)
+                {
+                    return;
+                }
+
+                CamImageBox.Image = ImageFrame;
+
+                using (Bitmap bitmap = ImageFrame.ToBitmap())
+                {
+                    if (bitmap == null)
+                    {
+                        return;
+                    }
+
+                    if (barcodeReader == null)
+                    {
+                        return;
+                    }
+
+                    var result = barcodeReader.Decode(bitmap);
+                    if (result != null && result.Text != lastQrCode)
+                    {
+                        lastQrCode = result.Text;
+                        processForward.Enqueue(result.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi xử lý khung hình: {ex.Message}");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                return;
+            }
+
+            if (captureInProgress)
+            {
+                btnStart.Text = "Start!";
+                Application.Idle -= ProcessFrame;
+                captureInProgress = false;
+            }
+            else
+            {
+                btnStart.Text = "Stop";
+                Application.Idle += ProcessFrame;
+                captureInProgress = true;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+            {
+                capture.Dispose();
+                capture = null;
+            }
+            cancellationTokenSource?.Cancel();
+            try
+            {
+                Task.WhenAll(HandllerPushTask, HandllerUITask).Wait(1000);
+            }
+            catch { }
+            cancellationTokenSource?.Dispose();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            ReleaseData();
+        }
+
+        private void CameraCapture_Load(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void CamImageBox_Click(object sender, EventArgs e)
+        {
+            ;
+        }
+    }
+}
